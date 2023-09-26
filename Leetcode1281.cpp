a0d2ae216f988b02f90ab7b27eb34c2693d3409d
Fahad Farooq <91177002+ffxhxd@users.noreply.github.com>
2023-09-26 17:06:30 +0530

    Update Leetcode1281.cpp

--- Leetcode1281.cpp
+++ Leetcode1281.cpp
@@ -3,17 +3,16 @@ using namespace std;
 
 int main(){
 	
-		int n;
-		cin>>n;
+	int n;
+	cin>>n;
         int sum=0;
         int prod=1;
 
         while(n != 0){
-
             int digit = n%10;
             prod = prod * digit;
             sum = sum + digit;
-           
+		
             n= n/10;
         }
 
